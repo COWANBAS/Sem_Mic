834cc988da05527a2e6779720d0ca117b9997340
Cowanbas <131304556+COWANBAS@users.noreply.github.com>
2025-01-29 18:43:57 -0300

    Add files via upload

---
+++ Microfone.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name           Desativar Microfone
+// @description    Desativa o acesso ao microfone aos sites.
+// @namespace      CowanSEMVOZ
+// @license        CowBas
+// @version        1.0
+// @author         Cowanbas
+// @match          *://*/*
+// @exclude        https://discord.com/*
+// @run-at         document-start
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  // Sobrescreve a função getUserMedia
+  navigator.mediaDevices.getUserMedia = function (constraints) {
+    return new Promise((resolve, reject) => {
+      if (constraints && constraints.audio) {
+        reject(new Error("Acesso negado."));
+      } else {
+        // Se o pedido não for para o usar o microfone, permite o acesso
+        navigator.mediaDevices.originalGetUserMedia(constraints).then(resolve).catch(reject);
+      }
+    });
+  };
+
+  // Salva a função original para permitir acesso a outras mídias (como vídeo) se necessário
+  navigator.mediaDevices.originalGetUserMedia = navigator.mediaDevices.getUserMedia.bind(navigator.mediaDevices);
+})();
